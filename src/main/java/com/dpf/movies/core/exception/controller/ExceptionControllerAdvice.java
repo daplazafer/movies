d433863c334c80576d9268d7e81def40babd7a2c
Daniel Plaza <dpf220691@gmail.com>
2020-01-27 16:01:59 +0100

    Fix exception handler

--- src/main/java/com/dpf/movies/core/exception/controller/ExceptionControllerAdvice.java
+++ src/main/java/com/dpf/movies/core/exception/controller/ExceptionControllerAdvice.java
@@ -4,13 +4,13 @@ import com.dpf.movies.core.base.BaseException;
 import com.dpf.movies.core.exception.dto.ExceptionDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ExceptionControllerAdvice {
 
     @ExceptionHandler(Exception.class)
